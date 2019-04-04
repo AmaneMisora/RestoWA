9ed4954f20ebefe865cc042f151eca95742b8ac4
Lucie Vallaude <lucie.vallade19@gmail.com>
2019-04-04 07:09:58 +0200

    Ajout check password

--- src/main/java/com/restowa/domain/model/UserAccountDAO.java
+++ src/main/java/com/restowa/domain/model/UserAccountDAO.java
@@ -5,7 +5,8 @@
  */
 package com.restowa.domain.model;
 
-import javax.persistence.EntityManager;
+import java.util.List;
+import javax.persistence.*;
 
 /**
  *
@@ -13,6 +14,22 @@ import javax.persistence.EntityManager;
  */
 public class UserAccountDAO {
     
+    /**
+     * Instancie un Useraccount tout en le rendant persistant dans la base de données
+     * @param em
+     * @param name
+     * @param lastname
+     * @param email
+     * @param password
+     * @param phoneNumber
+     * @param type
+     * @param Street
+     * @param City
+     * @param State
+     * @param ZipCode
+     * @param Country
+     * @return L'instance créée de Useraccount
+     */
     public static UserAccount creatUserAccount(EntityManager em, String name, String lastname, String email, String password, String phoneNumber, TypeEnum type, String Street, String City, String State, int ZipCode, String Country)
     {
         //instentiation
@@ -41,4 +58,52 @@ public class UserAccountDAO {
         
         return useraccount;
     }
+    
+    /**
+     * vérifie un login et un password
+     * @param em
+     * @param email
+     * @param password
+     * @return true si le compte existe et le password correcpond
+     */
+    public static boolean CheckLogin(EntityManager em, String email, String password)
+    {
+        List<UserAccount> userToCheck;
+        Query query = em.createQuery("select * from UserAccount where email = userEmail").setParameter("userEmail", email);
+	if(query.getResultList().size() == 0)
+            return false;
+        else
+        {
+            userToCheck = query.getResultList();
+            if(userToCheck.get(0).getPassword() == password)
+                return true;    
+        }
+                
+	return false	
+    }
+    
+    /*
+     * Met à jour les parametres d'un UserAccount
+     * (si un parametre n'est pas modifie, le mettre a null ou mettre son ancienne valeur ou une chaine vide)
+     * @param useraccount
+     * @param em
+     * @param name
+     * @param lastname
+     * @param email
+     * @param password
+     * @param phoneNumber
+     * @param type
+     * @param Street
+     * @param City
+     * @param State
+     * @param ZipCode
+     * @param Country
+     * @return la nouvelle instance de UserAccount
+     */
+    /*
+    public static UserAccount modifyUserAccount(UserAccount useraccount, EntityManager em, String name, String lastname, String email, String password, String phoneNumber, TypeEnum type, String Street, String City, String State, int ZipCode, String Country)
+    {
+    }
+    */
+    
 }
