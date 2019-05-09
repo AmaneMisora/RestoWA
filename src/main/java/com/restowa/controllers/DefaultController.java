00e2fbd0dce8339526a171542992d5e13eb3b079
Richard <yanisrichard@live.fr>
2019-05-10 00:35:55 +0200

    Verified working state :)

--- src/main/java/com/restowa/controllers/DefaultController.java
+++ src/main/java/com/restowa/controllers/DefaultController.java
@@ -24,39 +24,4 @@ public class DefaultController {
         LOGGER.log(Level.INFO, "End DefaultController");
         return "index";
     }
-    
-    public void initDb()
-    {
-        UserAccount useraccount;
-        Store store;
-        Address address;
-        OpeningHours openinghour;
-        
-        address = new Address("Street","City","State",12345,"Country");
-        useraccount = new UserAccount("Client1","lastname","email","password","phoneNumber",TypeEnum.Client,address);
-        uamanager.saveUserAccount(useraccount);
-        useraccount = new UserAccount("Client2","lastname","email","password","phoneNumber",TypeEnum.Client,address);
-        uamanager.saveUserAccount(useraccount);
-        useraccount = new UserAccount("Amin1","lastname","email","password","phoneNumber",TypeEnum.Administrateur,address);
-        uamanager.saveUserAccount(useraccount);
-        useraccount = new UserAccount("Owner1","lastname","email","password","phoneNumber",TypeEnum.Owner,address);
-        uamanager.saveUserAccount(useraccount);
-        useraccount = new UserAccount("Owner2","lastname","email","password","phoneNumber",TypeEnum.Owner,address);
-        uamanager.saveUserAccount(useraccount);
-        
-        /*
-        store = new Store();
-        store.setAddress(address);
-        store.setId(0);
-        store.setEmail("email");
-        store.setKeyStore("keyStore");
-        store.setLastModificationDate(LocalDateTime.MIN);
-        store.setLastModifiedBy(useraccount);
-        store.setLattitude(0.0);
-        store.setLongitude(0.0);
-        store.setName("name");
-        store.setOpeningHours(openingHours);
-*/
-
-    }
 }
