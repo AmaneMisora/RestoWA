61595ec0c516ef8faa71798cfd59b9f598361341
Lucie Vallaude <lucie.vallade19@gmail.com>
2019-05-09 17:47:44 +0200

    début init db

--- src/main/java/com/restowa/controllers/DefaultController.java
+++ src/main/java/com/restowa/controllers/DefaultController.java
@@ -3,7 +3,12 @@ package com.restowa.controllers;
 
 import com.restowa.DAO.UserAccountDAO;
 import com.restowa.bl.concrete.UserAccountManager;
+import com.restowa.domain.model.Address;
+import com.restowa.domain.model.OpeningHours;
+import com.restowa.domain.model.Store;
+import com.restowa.domain.model.TypeEnum;
 import com.restowa.domain.model.UserAccount;
+import java.time.LocalDateTime;
 import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -33,5 +38,40 @@ public class DefaultController {
         map.put("userName", ua.getName());
 */
         return "index";
+    }
+    
+    public void initDb()
+    {
+        UserAccount useraccount;
+        Store store;
+        Address address;
+        OpeningHours openinghour;
+        
+        address = new Address("Street","City","State",12345,"Country");
+        useraccount = new UserAccount("Client1","lastname","email","password","phoneNumber",TypeEnum.Client,address);
+        uamanager.saveUserAccount(useraccount);
+        useraccount = new UserAccount("Client2","lastname","email","password","phoneNumber",TypeEnum.Client,address);
+        uamanager.saveUserAccount(useraccount);
+        useraccount = new UserAccount("Amin1","lastname","email","password","phoneNumber",TypeEnum.Administrateur,address);
+        uamanager.saveUserAccount(useraccount);
+        useraccount = new UserAccount("Owner1","lastname","email","password","phoneNumber",TypeEnum.Owner,address);
+        uamanager.saveUserAccount(useraccount);
+        useraccount = new UserAccount("Owner2","lastname","email","password","phoneNumber",TypeEnum.Owner,address);
+        uamanager.saveUserAccount(useraccount);
+        
+        /*
+        store = new Store();
+        store.setAddress(address);
+        store.setId(0);
+        store.setEmail("email");
+        store.setKeyStore("keyStore");
+        store.setLastModificationDate(LocalDateTime.MIN);
+        store.setLastModifiedBy(useraccount);
+        store.setLattitude(0.0);
+        store.setLongitude(0.0);
+        store.setName("name");
+        store.setOpeningHours(openingHours);
+*/
+
     }
 }
